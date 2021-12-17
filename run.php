2b163266355d2ac29b82da1242959b432299c949
Nathan Schimmel <nathanjamesschimmel@gmail.com>
2021-12-17 13:14:53 -0600

    added embed

--- run.php
+++ run.php
@@ -5,6 +5,7 @@ use Discord\Discord;
 use Discord\WebSockets\Intents;
 use Discord\WebSockets\Event;
 use Discord\Builders\MessageBuilder;
+use Discord\Parts\Embed\Embed;
 require_once('mangadex.php');
 
 include __DIR__.'/vendor/autoload.php';
@@ -13,8 +14,6 @@ $config = parse_ini_file('config.ini', true);
 $settings = $config['SETTINGS'];
 $defaults = $config['DEFAULTS'];
 print_r($config);
-print_r($settings);
-print_r($defaults);
 
 $discord = new Discord([
 
@@ -43,9 +42,20 @@ $discord->on('ready', function ($discord){
         }
         if (str_starts_with($message->content, 'https://mangadex.org/title/')) {
             $id = explode('/', $message->content)[4];
-            $manga = new Manga($id, '');
+            $manga = new Manga('id', $id);
+            
+            $tags = implode(", ", $manga->tags);
+            $embed = new Embed($discord);
+            $embed->setTitle($manga->title)->setType('rich')
+                ->setColor('#000FFF')->setFooter($manga->status)
+                ->setURL($manga->url)->setImage($manga->cover_url)
+                ->addFieldValues('Author', $manga->author, true)
+                ->addFieldValues('Demographic', $manga->demographic, true)
+                ->addFieldValues('Rating', $manga->rating, true)
+                ->addFieldValues('Tags', $tags);
+
             $message->channel->sendMessage(MessageBuilder::new()
-               ->setContent($manga->title . "\n" . $manga->author));
+                ->addEmbed($embed));
         }
     });
 
